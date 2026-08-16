d562c235faf7ee0ad533ca65867948d6be6fd3a3
muijn <229912978@qq.com>
2026-08-16 19:08:49 +0800

    Add debounce function to utils

---
+++ miniprogram/utils/debounce.js
@@ -0,0 +1,17 @@
+function debounce(fn, delay = 300) {
+  let timer = null
+
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+
+    timer = setTimeout(() => {
+      fn.apply(this, args)
+    }, delay)
+  }
+}
+
+module.exports = {
+  debounce
+}
