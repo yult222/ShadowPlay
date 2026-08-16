9e44a62e103b81bbca49a6d49d54dd9775ec3c30
muijn <229912978@qq.com>
2026-08-16 19:26:31 +0800

    Add common-popup component with properties and methods

--- miniprogram/components/common-popup/index.js
+++ miniprogram/components/common-popup/index.js
@@ -1,1 +1,38 @@
+Component({
+  properties: {
+    visible: {
+      type: Boolean,
+      value: false
+    },
 
+    title: {
+      type: String,
+      value: '提示'
+    },
+
+    content: {
+      type: String,
+      value: ''
+    },
+
+    showCancel: {
+      type: Boolean,
+      value: true
+    },
+
+    confirmText: {
+      type: String,
+      value: '确定'
+    }
+  },
+
+  methods: {
+    onConfirm() {
+      this.triggerEvent('confirm')
+    },
+
+    onCancel() {
+      this.triggerEvent('cancel')
+    }
+  }
+})
